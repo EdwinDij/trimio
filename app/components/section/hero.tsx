6a80214ac60948c3ec641259af54a802b1b8be6f
EdwinDij <edwin.d899@gmail.com>
2025-07-20 18:09:22 +0200

    refactor: nettoyage du composant Hero, amélioration des styles et ajustement de la mise en page

--- app/components/section/hero.tsx
+++ app/components/section/hero.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Logo from "../../../public/LOGO-1.png";
 import Image from "next/image";
 import Link from "next/link";
-import { LucidePlayCircle  } from "lucide-react";
+import { LucidePlayCircle } from "lucide-react";
 
 export const Hero = () => {
   return (
     <section className="min-h-screen flex items-center justify-center relative overflow-hidden pt-20">
-      <div className="absolute inset-0 bg-gradient-to-r "></div>
+      <div className="absolute inset-0"></div>
+      <div className="absolute -bottom-10 -right-30 w-80 h-80 bg-white opacity-20 rounded-full blur-3xl pointer-events-none z-0"></div>
       <div className="relative z-10 text-center">
         <Image
           src={Logo}
@@ -29,15 +30,16 @@ export const Hero = () => {
         <div className="flex justify-center gap-6 items-center max-sm:flex-col max-sm:gap-10">
           <Link
             href="#contact"
-            className="bg-amber-500/90 text-[#102a47] px-6 py-3 rounded-full font-semibold hover:bg-amber-500/70 transition-colors"
+            className="bg-amber-500/90 text-[#102a47] px-6 py-3 rounded-full font-semibold hover:bg-amber-500/70 transition-colors hover:scale-110 "
           >
             Démarrer un projet
           </Link>
           <Link
             href="#services"
-            className="flex items-center gap-2 bg-transparent border px-6 py-3 rounded-full font-semibold text-white hover:bg-white/10 transition-colors"
+            className="flex items-center gap-2 bg-transparent border px-6 py-3 rounded-full font-semibold text-white hover:bg-white/10 transition-colors hover:text-black"
           >
-            <LucidePlayCircle/>Démarrer un projet
+            <LucidePlayCircle />
+            Démarrer un projet
           </Link>
         </div>
       </div>
